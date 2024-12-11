3a3bd5cda1735e6861645e323c6ec007a159d1cd
Saliinger <alexisnoukan@gmail.com>
2024-12-11 02:00:46 +0100

    fix exit

--- src/exec/ft_exec_builtins.c
+++ src/exec/ft_exec_builtins.c
@@ -4,19 +4,19 @@ int ft_exec_builtins(t_minishell *minishell, t_command *command)
 {
     int exit = 0;
 
-    if (command->id == 1)
+    if (command->id == ECHO_ID)
         exit = ft_echo(command);
-    else if (command->id == 2)
+    else if (command->id == CD_ID)
         exit = ft_cd(command, minishell);
-    else if (command->id == 4)
+    else if (command->id == PWD_ID)
         exit = ft_pwd(minishell);
-    else if (command->id == 5)
+    else if (command->id == EXPORT_ID)
         exit = ft_export(command, minishell);
-    else if (command->id == 6)
+    else if (command->id == UNSET_ID)
         exit = ft_unset(command, minishell);
-    else if (command->id == 7)
+    else if (command->id == ENV_ID)
         exit = ft_env(minishell);
-    else if (command->id == 8)
+    else if (command->id == EXIT_ID)
         ft_exit(minishell, command, true);
     return (exit);
 }
