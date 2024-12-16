bcdbf238488aabe811a35fe891febd6daa88fabd
Saliinger <alexisnoukan@gmail.com>
2024-12-16 18:50:59 +0100

    norm

--- src/parsing/3_quotes/remove_quote.c
+++ src/parsing/3_quotes/remove_quote.c
@@ -6,47 +6,47 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 23:02:35 by anoukan           #+#    #+#             */
-/*   Updated: 2024/12/10 12:52:49 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/12/16 18:50:51 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
 // remove external quote
+char	*extend_remove_quote(char *s)
+{
+	int		j;
+	char	*res;
+	int		status;
+
+	j = 0;
+	status = 0;
+	res = safe_strdup("", ALLOC_COMMAND);
+	while (s[j])
+	{
+		if ((s[j] == '\'' || s[j] == '\"') && status == 0)
+			status = in_quote(status, s[j]);
+		else if ((s[j] == '\'' && status == -1) || (s[j] == '\"'
+				&& status == 1))
+			status = in_quote(status, s[j]);
+		else
+			res = add_char(res, s[j]);
+		j++;
+	}
+	return (res);
+}
 
 char	**remove_quote(char **arg)
 {
 	int		i;
-	int		j;
 	char	**res;
-	int		status;
 
 	i = 0;
-	res = (char **)safe_malloc(sizeof(char *) * (nbr_of_line(arg) + 1), ALLOC_COMMAND);
+	res = (char **)safe_malloc(sizeof(char *) * (nbr_of_line(arg) + 1),
+			ALLOC_COMMAND);
 	while (arg[i])
 	{
-		j = 0;
-		status = 0;
-		res[i] = safe_strdup("", ALLOC_COMMAND);
-		while (arg[i][j])
-		{
-			if ((arg[i][j] == '\'' || arg[i][j] == '\"') && status == 0)
-			{
-				status = in_quote(status, arg[i][j]);
-				j++;
-			}
-			else if ((arg[i][j] == '\'' && status == -1) || (arg[i][j] == '\"'
-					&& status == 1))
-			{
-				status = in_quote(status, arg[i][j]);
-				j++;
-			}
-			else
-			{
-				res[i] = add_char(res[i], arg[i][j]);
-				j++;
-			}
-		}
+		res[i] = extend_remove_quote(arg[i]);
 		i++;
 	}
 	res[i] = NULL;
