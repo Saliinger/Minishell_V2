4cd9a4fd5ba98917b9a7c64821728c8c700fa0b5
Saliinger <alexisnoukan@gmail.com>
2024-12-16 01:38:12 +0100

    chat gpt solution

--- src/exec/exec_processor.c
+++ src/exec/exec_processor.c
@@ -35,38 +35,70 @@ int	get_fd(t_redir *redir)
 	return (fd);
 }
 
-static int	handle_redirections(t_command *cmd)
+static int handle_redirections(t_command *cmd)
 {
-	t_redir	*redir;
-	int		fd;
+	t_redir *redir;
+	int     fd;
 
 	redir = cmd->redirection;
 	if (!redir)
 		return (0);
+
 	while (redir)
 	{
-		fd = get_fd(redir);
+		fd = get_fd(redir); // Open the file/pipes here
 		if (fd == -1)
 			return (-1);
+
 		if (redir->type == R_INPUT || redir->type == R_HEREDOC)
 		{
-			if (redir->next && redir->next->type == R_HEREDOC)
-				;
-			else
-				if (dup2(fd, STDIN_FILENO) < 0)
-					return (perror("dup2 (entrée)"), close(fd), -1);
+			if (dup2(fd, STDIN_FILENO) < 0)
+				return (perror("dup2 (input)"), close(fd), -1);
 		}
 		else
 		{
 			if (dup2(fd, STDOUT_FILENO) < 0)
-				return (perror("dup2 (sortie)"), close(fd), -1);
+				return (perror("dup2 (output)"), close(fd), -1);
 		}
-		close(fd);
+		close(fd); // Close after duplication
 		redir = redir->next;
 	}
-	unlink("temp_heredoc_file");
 	return (0);
 }
+
+
+// static int	handle_redirections(t_command *cmd)
+// {
+// 	t_redir	*redir;
+// 	int		fd;
+//
+// 	redir = cmd->redirection;
+// 	if (!redir)
+// 		return (0);
+// 	while (redir)
+// 	{
+// 		fd = get_fd(redir);
+// 		if (fd == -1)
+// 			return (-1);
+// 		if (redir->type == R_INPUT || redir->type == R_HEREDOC)
+// 		{
+// 			if (redir->next && redir->next->type == R_HEREDOC)
+// 				;
+// 			else
+// 				if (dup2(fd, STDIN_FILENO) < 0)
+// 					return (perror("dup2 (entrée)"), close(fd), -1);
+// 		}
+// 		else
+// 		{
+// 			if (dup2(fd, STDOUT_FILENO) < 0)
+// 				return (perror("dup2 (sortie)"), close(fd), -1);
+// 		}
+// 		close(fd);
+// 		redir = redir->next;
+// 	}
+// 	unlink("temp_heredoc_file");
+// 	return (0);
+// }
 
 // potentiel solution
 // static int	handle_redirections(t_command *cmd)
@@ -101,12 +133,128 @@ static int	handle_redirections(t_command *cmd)
 // 	return (0);
 // }
 
-void	process_fork(t_command *cmd, t_minishell *m)
+bool is_hd(t_redir *redir)
 {
-	int		prev_pipe_fd;
-	pid_t	pid;
+	while (redir)
+	{
+		if (redir->type == R_HEREDOC)
+			return (true);
+		redir = redir->next;
+	}
+	return (false);
+}
 
-	prev_pipe_fd = -1;
+int preprocess_heredocs(t_command *cmd)
+{
+	t_redir *redir;
+	int     hd_id = 0; // Unique identifier for heredocs
+
+	while (cmd)
+	{
+		redir = cmd->redirection;
+		while (redir)
+		{
+			if (redir->type == R_HEREDOC)
+			{
+				char temp_file[128];
+				snprintf(temp_file, sizeof(temp_file), "temp_hd_%d", hd_id++);
+				int fd = open(temp_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				if (fd < 0)
+					return (perror("Erreur ouverture heredoc"), -1);
+
+				char *line;
+				while (1)
+				{
+					line = readline("> ");
+					if (!line || ft_strcmp(line, redir->redir) == 0)
+						break;
+					write(fd, line, ft_strlen(line));
+					write(fd, "\n", 1);
+					free(line);
+				}
+				close(fd);
+				free(line);
+
+				// Replace heredoc redirection with input from the temp file
+				free(redir->redir);
+				redir->redir = ft_strdup(temp_file);
+				redir->type = R_INPUT; // Change type to standard input redirection
+			}
+			redir = redir->next;
+		}
+		cmd = cmd->subcommand;
+	}
+	return (0);
+}
+
+
+
+// void	process_fork(t_command *cmd, t_minishell *m, int *status)
+// {
+// 	int		prev_pipe_fd;
+// 	pid_t	pid;
+//
+// 	prev_pipe_fd = -1;
+// 	while (cmd)
+// 	{
+// 		if (cmd->subcommand)
+// 		{
+// 			if (pipe(cmd->pipe_fds) < 0)
+// 			{
+// 				perror("Erreur de pipe");
+// 				break ;
+// 			}
+// 		}
+// 		if (preprocess_heredocs(cmd) < 0)
+// 		{
+// 			printerr("minishell: error processing heredocs\n");
+// 			return;
+// 		}
+//
+// 		pid = fork();
+// 		if (pid < 0)
+// 		{
+// 			perror("Erreur lors du fork");
+// 			break ;
+// 		}
+// 		if (pid == 0)
+// 		{
+// 			if (prev_pipe_fd != -1)
+// 				dup2(prev_pipe_fd, STDIN_FILENO);
+// 			if (cmd->subcommand)
+// 				dup2(cmd->pipe_fds[1], STDOUT_FILENO);
+// 			if (handle_redirections(cmd) < 0)
+// 				exit(EXIT_FAILURE);
+// 			if (prev_pipe_fd != -1)
+// 				close(prev_pipe_fd);
+// 			if (cmd->subcommand)
+// 			{
+// 				close(cmd->pipe_fds[0]);
+// 				close(cmd->pipe_fds[1]);
+// 			}
+// 			exit(ft_exec(m, cmd));
+// 		}
+// 		if (prev_pipe_fd != -1)
+// 			close(prev_pipe_fd);
+// 		if (cmd->subcommand)
+// 			close(cmd->pipe_fds[1]);
+// 		prev_pipe_fd = cmd->pipe_fds[0];
+// 		cmd = cmd->subcommand;
+// 	}
+// }
+
+void process_fork(t_command *cmd, t_minishell *m, int *status)
+{
+	int prev_pipe_fd = -1; // File descriptor for the previous command's output
+	pid_t pid;
+
+	// Preprocess heredocs for the entire command chain
+	if (preprocess_heredocs(cmd) < 0)
+	{
+		printerr("minishell: error processing heredocs\n");
+		return;
+	}
+
 	while (cmd)
 	{
 		if (cmd->subcommand)
@@ -114,23 +262,28 @@ void	process_fork(t_command *cmd, t_minishell *m)
 			if (pipe(cmd->pipe_fds) < 0)
 			{
 				perror("Erreur de pipe");
-				break ;
+				return;
 			}
 		}
+
 		pid = fork();
 		if (pid < 0)
 		{
 			perror("Erreur lors du fork");
-			break ;
+			return;
 		}
-		else if (pid == 0)
+
+		if (pid == 0) // Child process
 		{
-			if (prev_pipe_fd != -1)
+			if (prev_pipe_fd != -1) // Read from previous pipe
 				dup2(prev_pipe_fd, STDIN_FILENO);
-			if (cmd->subcommand)
+			if (cmd->subcommand) // Write to the current pipe
 				dup2(cmd->pipe_fds[1], STDOUT_FILENO);
+
 			if (handle_redirections(cmd) < 0)
 				exit(EXIT_FAILURE);
+
+			// Close pipe ends in the child
 			if (prev_pipe_fd != -1)
 				close(prev_pipe_fd);
 			if (cmd->subcommand)
@@ -138,16 +291,28 @@ void	process_fork(t_command *cmd, t_minishell *m)
 				close(cmd->pipe_fds[0]);
 				close(cmd->pipe_fds[1]);
 			}
-			exit(ft_exec(m, cmd));
+
+			exit(ft_exec(m, cmd)); // Execute the command
 		}
+
+		// Parent process
 		if (prev_pipe_fd != -1)
-			close(prev_pipe_fd);
+			close(prev_pipe_fd); // Close the previous pipe's read end
 		if (cmd->subcommand)
-			close(cmd->pipe_fds[1]);
-		prev_pipe_fd = cmd->pipe_fds[0];
+			close(cmd->pipe_fds[1]); // Close the current pipe's write end
+		prev_pipe_fd = cmd->pipe_fds[0]; // Save the current pipe's read end
+
 		cmd = cmd->subcommand;
 	}
+
+	// Wait for all child processes
+	while (waitpid(-1, status, 0) > 0)
+	{
+		if (WIFEXITED(*status))
+			m->exit_status[0] = WEXITSTATUS(*status);
+	}
 }
+
 
 void	process_input_line(char *line, t_minishell *m)
 {
@@ -162,7 +327,7 @@ void	process_input_line(char *line, t_minishell *m)
 		ft_exec(m, cmd);
 	else
 	{
-		process_fork(cmd, m);
+		process_fork(cmd, m, &status);
 		while (waitpid(-1, &status, 0) > 0)
 		{
 			if (WIFEXITED(status))
