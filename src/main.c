160b2ef48fbecc0e31757bbf34f917e19b3a4178
ekrebs <ekrebs.contact@gmail.com>
2024-12-17 01:54:36 +0100

    fix typo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:32:10 by anoukan           #+#    #+#             */
-/*   Updated: 2024/12/17 01:49:26 by ekrebs           ###   ########.fr       */
+/*   Updated: 2024/12/17 01:53:38 by ekrebs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int	main(int ac, char **av, char **env)
 	set_signals_to_minishell();
 	(void)av;
 	if (ac > 1 || isatty(STDIN_FILENO) == 0 || isatty(STDOUT_FILENO) == 0)
-		return (printerr("err: case not asked by subject\n."), 1);
+		return (printerr("err: case not asked by subject.\n"), 1);
 	exit_status = 0;
 	minishell = init(env, getcwd(buffer, 4096), &exit_status);
 	if (!minishell)
