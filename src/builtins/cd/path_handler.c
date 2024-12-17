f64e0c53adf2821b6dd79d2293dee48228918c92
ekrebs <ekrebs.contact@gmail.com>
2024-12-17 05:49:54 +0100

    + set back isatty() tests
    + removed comment in cd/path_handler.c
    	we are now nominated !

--- src/builtins/cd/path_handler.c
+++ src/builtins/cd/path_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   path_handler.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 15:31:01 by anoukan           #+#    #+#             */
-/*   Updated: 2024/12/17 04:44:57 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/12/17 05:47:08 by ekrebs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,6 @@ void	add_path(char *dest, char *to_add)
 		return ;
 	len = ft_strlen(dest);
 	if (len > 0 && dest[len - 1] != '/')
-		ft_strlcat(dest, "/", PATH_MAX); // Add '/' only if needed
+		ft_strlcat(dest, "/", PATH_MAX);
 	ft_strlcat(dest, to_add, PATH_MAX);
 }
--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:32:10 by anoukan           #+#    #+#             */
-/*   Updated: 2024/12/17 05:40:05 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/12/17 05:45:05 by ekrebs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,8 @@ int	main(int ac, char **av, char **env)
 
 	set_signals_to_minishell();
 	(void)av;
-	// if (ac > 1 || isatty(STDIN_FILENO) == 0 || isatty(STDOUT_FILENO) == 0)
-	// 	return (printerr("err: case not asked by subject.\n"), 1);
-	(void)ac;
+	if (ac > 1 || isatty(STDIN_FILENO) == 0 || isatty(STDOUT_FILENO) == 0)
+		return (printerr("err: case not asked by subject.\n"), 1);
 	exit_status = 0;
 	minishell = init(env, getcwd(buffer, 4096), &exit_status);
 	if (!minishell)
