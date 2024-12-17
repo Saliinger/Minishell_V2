250eaddf4d7459f7e246ee20bfdf3a87f7a87fcb
Saliinger <alexisnoukan@gmail.com>
2024-12-17 03:06:56 +0100

    fix invalid read

--- src/builtins/cd/path_constructor.c
+++ src/builtins/cd/path_constructor.c
@@ -6,13 +6,14 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 12:37:06 by anoukan           #+#    #+#             */
-/*   Updated: 2024/12/17 02:53:43 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/12/17 03:06:47 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-// Initialize path by constructing the base path
+// init path handle the start of the path that we wan't to go to ~ / or nothing
+
 static int	init_path(char *in, t_minishell *minishell, char **new_in)
 {
 	char	*temp;
@@ -23,68 +24,72 @@ static int	init_path(char *in, t_minishell *minishell, char **new_in)
 	return (0);
 }
 
-// Check if a string contains dots only and return a boolean
 bool	check_dot(char *in)
 {
 	int	i;
+	int	nbr;
 
-	if (!in)
-		return (false);
 	i = 0;
+	nbr = 0;
+	if (!in)
+		return (0);
 	while (in[i])
 	{
-		if (in[i] != '.')
-			return (false);
+		if (in[i] == '.')
+			nbr++;
+		while (in[i] == '.')
+			i++;
 		i++;
 	}
-	return (true);
+	return (nbr);
 }
 
-// Count the number of consecutive dots at the start of the string
 int	nbr_of_dot(char *line)
 {
-	int	i;
+	int	nbr;
 
-	if (!line)
-		return (0);
-	i = 0;
-	while (line[i] && line[i] == '.')
-		i++;
-	return (i);
+	nbr = 0;
+	while (line[nbr] == '.')
+		nbr++;
+	return (nbr);
 }
 
-// Safely return the final resolved path
 char	*get_res(char *current_path)
 {
+	char	*res;
+
+	res = NULL;
 	if (ft_strlen(current_path) > 0)
-		return (safe_strdup(current_path, ALLOC_COMMAND));
-	return (safe_strdup("/", ALLOC_COMMAND));
+		res = (char *)safe_strdup(current_path, ALLOC_COMMAND);
+	else
+		res = (char *)safe_strdup("/", ALLOC_COMMAND);
+	current_path = NULL;
+	return (res);
 }
 
-// Main path constructor logic
 char	*path_constructor(t_minishell *minishell, char *in)
 {
 	char	*res;
 	char	*new_in;
 	char	*current_path;
-	char	**in_cut;
 	int		i;
+	char	**in_cut;
 
-	init_path(in, minishell, &new_in);
-	if (!ft_strchr(new_in, '.'))
+	i = init_path(in, minishell, &new_in);
+	if (!check_dot(new_in))
 		return (new_in);
 	in_cut = ft_split_safe(new_in, '/', ALLOC_COMMAND);
-	current_path = (char *)safe_malloc(sizeof(char) * (PATH_MAX + 1),
+	current_path = (char *)safe_malloc(sizeof(char) * PATH_MAX + 1,
 			ALLOC_COMMAND);
-	*current_path = '\0';
-	i = 0;
 	while (in_cut[i])
 	{
 		if (check_dot(in_cut[i]))
+		{
 			if (nbr_of_dot(in_cut[i]) == 2)
 				remove_path(current_path);
-			else if (!check_dot(in_cut[i]) || nbr_of_dot(in_cut[i]) == 1)
-					add_path(current_path, in_cut[i]);
+		}
+		else
+			add_path(current_path, in_cut[i]);
 		i++;
 	}
 	res = get_res(current_path);
